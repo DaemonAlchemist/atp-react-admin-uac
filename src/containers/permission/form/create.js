185352aa35264abb6741dbaa827880a67e28c8f7
Andrew Wittrock <andy@wittrock.us>
2017-04-21 23:06:16 -0500

    Refresh permissionlist after creating

--- src/containers/permission/form/create.js
+++ src/containers/permission/form/create.js
@@ -8,5 +8,9 @@ import NewPermissionForm from "../../../components/permission/form/create";
 
 export default reduxForm({
     form: 'new-permission-form',
-    onSubmit: (data, dispatch) => dispatch(Permission().action.post(data))
+    onSubmit: (data, dispatch) => dispatch((dispatch, getState) => {
+        Permission().action.post(data)(dispatch, getState).then(data => {
+            Permission().action.list({})(dispatch, getState);
+        });
+    })
 })(NewPermissionForm);
