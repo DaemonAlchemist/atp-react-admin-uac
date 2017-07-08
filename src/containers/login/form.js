767e16ffc3ff9175477f3dd27ba6b45742d00023
Andrew Wittrock <andy@wittrock.us>
2017-07-08 12:04:10 -0500

    Add support for additional login fields

--- src/containers/login/form.js
+++ src/containers/login/form.js
@@ -5,9 +5,11 @@
 import LoginForm from "../../components/login/form";
 import {reduxForm} from "redux-form";
 
+import config from "atp-config";
 import {login} from "../../reducer/login";
 
 export default reduxForm({
     form: 'atp-uac/login-form',
-    onSubmit: (credentials, dispatch) => dispatch(login(credentials))
+    onSubmit: (credentials, dispatch) => dispatch(login(credentials)),
+    initialValues: config.get('login.additionalFields')
 })(LoginForm);
