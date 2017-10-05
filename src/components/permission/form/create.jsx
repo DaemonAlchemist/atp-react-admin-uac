7600b329d970e27c2529f6147405aa5bc085d63c
Andrea <andrea@wittrock.us>
2017-10-04 23:14:28 -0500

    Fix permission create input name

--- src/components/permission/form/create.jsx
+++ src/components/permission/form/create.jsx
@@ -9,7 +9,7 @@ import {Button, InputGroup} from "react-bootstrap";
 export default props =>
     <form onSubmit={props.handleSubmit}>
         <InputGroup>
-            <Field name="permissionName" placeholder="Permission name" component="input" className="form-control"/>
+            <Field name="name" placeholder="Permission name" component="input" className="form-control"/>
             <InputGroup.Button>
                 <Button bsStyle="primary" type="submit"><i className="fa fa-plus"/> Create</Button>
             </InputGroup.Button>
