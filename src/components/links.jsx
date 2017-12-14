c1863e958846d45e5a9653ed3e6866399056a7a7
Andrew Wittrock <andy@wittrock.us>
2017-12-13 23:58:53 -0600

    Show loading indicators for user links

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -4,13 +4,14 @@ import {Link} from 'atp-react-tab-router';
 import {User} from "../reducer/user";
 import {get} from 'atp-pointfree';
 
-export const UserLink = props =>
-    <Link
+export const UserLink = props => props.user
+    ? <Link
         {...props}
         to={`/uac/user/${props.user.id}`}
         label={<span><i className="fa fa-user" /> User "{props.user.userName}"</span>}
         target="new"
-    />;
+      />
+    : <div><i className="fa fa-spinner fa-spin" /> Loading...</div>;
 
 export const UserLinkFull = connectWithLifecycle(
     (state, props) => ({
@@ -19,8 +20,9 @@ export const UserLinkFull = connectWithLifecycle(
     (dispatch, props) => ({
         componentDidMount: () => {dispatch(User().action.fetch(props.userId));}
     })
-)(({user}) =>
-    <UserLink user={user}>
+)(({user}) => user
+    ? <UserLink user={user}>
         <i className="fa fa-user"/> {user.userName}
-    </UserLink>
+     </UserLink>
+    : <div><i className="fa fa-spinner fa-spin" /> Loading...</div>
 );
