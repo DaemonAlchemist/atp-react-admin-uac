089977f393e507a7f34c99174527eb5e5765a406
Andrew Wittrock <andy@wittrock.us>
2018-01-22 23:56:33 -0600

    Hook up change password button

--- src/components/user/details.jsx
+++ src/components/user/details.jsx
@@ -1,31 +1,40 @@
 
 import React from "react";
-import {Col, Row} from "react-bootstrap";
+import {Col, Row, Button} from "react-bootstrap";
 import {InlineEdit} from "atp-inline-edit";
 import {Assigner} from "atp-ui";
 import {Icon} from 'react-font-awesome-5';
 
-export default props => props.user ? <Row>
+const toggleGroup = "atpUacUserModal";
+const toggleId = "changePassword";
+
+export default ({user, saveUserName, isPasswordModalVisible, showPasswordModal, changePassword, allRoles, userRoles, joinRole, leaveRole}) => user ? <Row>
     <Col xs={12} sm={6} md={4}>
         <h1 style={{marginTop: 0}}>
             <InlineEdit.Text
                 id="user.name.edit"
-                value={props.user.userName}
+                value={user.userName}
                 name="userName"
-                onSave={props.saveUserName}
+                onSave={saveUserName}
             />
         </h1>
     </Col>
+    <Col xs={12} sm={6} md={8} className="text-right">
+        <Button bsSize="lg" onClick={showPasswordModal}>
+            <Icon.Lock/> Change password
+        </Button>
+        {isPasswordModalVisible && <Button onClick={changePassword}>Change password modal goes here.</Button>}
+    </Col>
     <Col xs={12}>
-        {props.allRoles && props.userRoles
+        {allRoles && userRoles
             ? <Assigner
                 Icon={Icon.Sitemap}
                 label="Roles"
                 formId="userRolesAssigner"
-                available={props.allRoles}
-                assigned={props.userRoles}
-                onAssign={props.joinRole}
-                onUnassign={props.leaveRole}
+                available={allRoles}
+                assigned={userRoles}
+                onAssign={joinRole}
+                onUnassign={leaveRole}
               />
             : <span><Icon.Spinner spin /> Roles loading...</span>
         }
--- src/containers/user/details.js
+++ src/containers/user/details.js
@@ -3,18 +3,28 @@ import {connect} from "react-redux";
 import UserDetails from "../../components/user/details";
 import {User, UserRole} from "../../reducer/user";
 import {Role} from "../../reducer/role";
+import {toggle} from 'atp-ui';
+import {get} from 'atp-pointfree';
+
+const passwordModalToggleId = 'user.details.password.modal';
 
 export default connect(
     (state, props) => ({
-        user: User().select.one(() => state, props.userId),
-        allRoles: Role().select.all(() => state),
+        user: User().select.one(get(state), props.userId),
+        allRoles: Role().select.all(get(state)),
         userRoles: props.userId
-            ? Role().select.byIdList(() => state, User().roles.select.all(() => state, props.userId))
-            : []
+            ? Role().select.byIdList(get(state), User().roles.select.all(get(state), props.userId))
+            : [],
+        isPasswordModalVisible: toggle.isVisible(get(state), passwordModalToggleId),
     }),
     (dispatch, props) => ({
         saveUserName: (data, dispatch) => {dispatch(User().action.update(props.userId, data));},
         joinRole: role => dispatch(User().roles.action.post(props.userId, role.id)),
-        leaveRole: role => dispatch(User().roles.action.delete(props.userId, role.id))
+        leaveRole: role => dispatch(User().roles.action.delete(props.userId, role.id)),
+        showPasswordModal: () => {dispatch(toggle.show(passwordModalToggleId))},
+        changePassword: () => {
+            dispatch(toggle.hide(passwordModalToggleId));
+            //TODO:  Save password
+        }
     })
 )(UserDetails);
