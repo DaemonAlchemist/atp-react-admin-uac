4a1dc3d0c8e2fdfa0c89dbc5fcc8fe8e2956f43e
Andrew Wittrock <andy@wittrock.us>
2018-07-28 11:28:58 -0500

    Upgrade HasPermission component to allow permission negation

--- src/containers/HasPermission.jsx
+++ src/containers/HasPermission.jsx
@@ -7,8 +7,10 @@ import {identity} from 'atp-pointfree';
 export default connect(
     state => ({userPermissions: state.uac.profile.permissions}),
     dispatch => ({})
-)(({permissions, userPermissions, children}) =>
-    !permissions || userPermissions && a(userPermissions).intersect(permissions).length > 0
+)(({permissions, userPermissions, yes, no, children}) => {
+    const hasPermission = !permissions || userPermissions && a(userPermissions).intersect(permissions).length > 0;
+
+    return hasPermission && (yes || !yes && !no) || !hasPermission && no
         ? React.Children.map(children, identity)
-        : null
-);
+        : null;
+});
